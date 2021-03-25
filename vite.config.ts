c24903c8f065ae5e3b634c95e52dc751908805aa
hyrious <hyrious@outlook.com>
2021-03-25 09:02:37 +0800

    wip

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-    build: {
-        lib: {
-            entry: "src/index.ts",
-            formats: ["iife", "es"],
-            name: "Syntax",
-        },
+  build: {
+    lib: {
+      entry: "src/index.ts",
+      formats: ["iife", "es"],
+      name: "Syntax",
     },
+  },
 });
